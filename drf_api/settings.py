a2fc30a292ae0b20ea104b34633b67e9e24c220e
pakkONE <johan.pakarinen@gmail.com>
2022-07-12 12:37:02 +0000

    added FollowerView and FollowerDetail with serializer

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'profiles',
     'comments',
     'likes',
+    'followers',
 ]
 
 MIDDLEWARE = [
---
+++ followers/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerializer
+
+
+class FollowerList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
